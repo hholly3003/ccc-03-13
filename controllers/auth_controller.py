e15df644900b63d9ff8b86bab8531bc5e30881e2
Holly <h.holly3003@gmail.com>
2020-11-19 04:50:28 +1100

    added in bcrypt

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,9 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, abort, jsonify, request
 from schemas.UserSchema import user_schema
 from models.User import User
+from main import db, bcrypt
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
 @auth.route("/register", methods=["POST"])
 def auth_register():
-    return "working"
+    user_fields = user_schema.load(request.json)
+
+    user = User.query.filter_by(email=user_fields["email"]).first()
+
+    if user:
+        return abort(400, description="Email already registered")
+    
+    user = User()
+    user.email = user_fields["email"]
+    #user.password = user_fields["password"]
+    user.password = bcrypt.generate_password_hash(user_fields["password"]).decode("utf-8")
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user_schema.dump(user))
